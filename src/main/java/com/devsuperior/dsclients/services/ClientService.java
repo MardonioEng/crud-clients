fffe06e53c3c8025e3907427042daeeaf940380d
Mardonio Rodrigues <mardoniorodrigues11@gmail.com>
2022-02-26 09:06:59 -0300

    Criado rota GET /clients/id

--- src/main/java/com/devsuperior/dsclients/services/ClientService.java
+++ src/main/java/com/devsuperior/dsclients/services/ClientService.java
@@ -3,10 +3,14 @@ package com.devsuperior.dsclients.services;
 import com.devsuperior.dsclients.dto.ClientDTO;
 import com.devsuperior.dsclients.entities.Client;
 import com.devsuperior.dsclients.repositories.ClientRepository;
+import com.devsuperior.dsclients.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 public class ClientService {
@@ -14,10 +18,17 @@ public class ClientService {
     @Autowired
     private ClientRepository clientRepository;
 
-
+    @Transactional(readOnly = true)
     public Page<ClientDTO> findAllPaged(PageRequest pageRequest) {
         Page<Client> list = clientRepository.findAll(pageRequest);
         return list.map(ClientDTO::new);
+    }
+
+    @Transactional(readOnly = true)
+    public ClientDTO findById(Long id) {
+        Optional<Client> obj = clientRepository.findById(id);
+        Client entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found."));
+        return new ClientDTO(entity);
     }
 
 }
